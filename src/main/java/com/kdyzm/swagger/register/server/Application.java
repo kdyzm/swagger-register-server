a6adb25e176edb2c3b5378a3419713a57b978cbc
kdyzm <kdyzm@foxmail.com>
2021-07-02 11:14:12 +0800

    集成knife4j

---
+++ src/main/java/com/kdyzm/swagger/register/server/Application.java
@@ -0,0 +1,16 @@
+package com.kdyzm.swagger.register.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kdyzm
+ * @date 2021/7/1
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
