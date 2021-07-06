c0726a4fe587e9a3f596f92def1f2324f6231a7b
kdyzm <kdyzm@foxmail.com>
2021-07-06 10:00:45 +0800

    提交

--- src/main/java/com/kdyzm/swagger/register/server/controller/SwaggerRegisterController.java
+++ src/main/java/com/kdyzm/swagger/register/server/controller/SwaggerRegisterController.java
@@ -6,6 +6,8 @@ import com.kdyzm.swagger.register.server.entity.GroupInfo;
 import com.kdyzm.swagger.register.server.entity.SwaggerJson;
 import com.kdyzm.swagger.register.server.mapper.IGroupInfoMapper;
 import com.kdyzm.swagger.register.server.mapper.ISwaggerJsonMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +22,14 @@ import java.util.Objects;
 @RequestMapping("/swagger")
 @Slf4j
 @AllArgsConstructor
+@Api(tags = {"swagger注册中心接口集合"})
 public class SwaggerRegisterController {
 
     private ISwaggerJsonMapper swaggerJsonMapper;
 
     private IGroupInfoMapper groupInfoMapper;
 
+    @ApiOperation("swagger注册接口")
     @PostMapping("/regist")
     @ResponseBody
     public Boolean regist(@RequestBody RegisterInfo registerInfo) {
@@ -51,6 +55,7 @@ public class SwaggerRegisterController {
         return true;
     }
 
+    @ApiOperation(value = "获取swagger注册详情接口",notes = "Knife4j根据此接口获取接口列表详情并渲染")
     @GetMapping("/detail")
     @ResponseBody
     public String getSwaggerDetail(@RequestParam("groupName") String groupName) {
