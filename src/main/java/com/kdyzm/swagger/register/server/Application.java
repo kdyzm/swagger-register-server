857d81e11ac7b0b5528ce6374f85f42488b40b6a
kdyzm <kdyzm@foxmail.com>
2021-10-16 14:12:31 +0800

    降低nacos版本号&降低springboot版本号

--- src/main/java/com/kdyzm/swagger/register/server/Application.java
+++ src/main/java/com/kdyzm/swagger/register/server/Application.java
@@ -3,6 +3,7 @@ package com.kdyzm.swagger.register.server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -15,6 +16,7 @@ import java.net.UnknownHostException;
  */
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) throws UnknownHostException {
